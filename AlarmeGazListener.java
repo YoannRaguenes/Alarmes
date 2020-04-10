269f95cb2dfa753e52621ddd031bcfe073bdb71e
YoannRaguenes <58259678+YoannRaguenes@users.noreply.github.com>
2020-04-10 15:41:58 +0200

    Update AlarmeGazListener.java

--- AlarmeGazListener.java
+++ AlarmeGazListener.java
@@ -1,4 +1,7 @@
 
-public interface AlarmeGazListener {
+import java.util.Date;
+import java.util.EventListener;
 
+public interface AlarmeGazListener extends EventListener  {
+	public String receptionGaz(Date dateApparition, String localisation,int type,int importance);
 }
