2ce13ab6f3119a0bd129b2eeac780c260b96c6d7
YoannRaguenes <58259678+YoannRaguenes@users.noreply.github.com>
2020-04-10 15:41:37 +0200

    Update AlarmeGaz.java

--- AlarmeGaz.java
+++ AlarmeGaz.java
@@ -1,8 +1,31 @@
+import java.util.ArrayList;
+import java.util.Date;
 
-public class AlarmeGaz {
-	private String date;
-    private String localisation;
-    private int importance;
-    private String typeGaz;
+
+public class AlarmeGaz implements AlarmeGazListener {
+	static private float seuil;
+    
+    public static float getSeuil() {
+		return seuil;
+	}
+
+	public static void setSeuil(float seuil) {
+		AlarmeGaz.seuil = seuil;
+	}
+	
+public static ArrayList<String> listeGaz = new ArrayList<String>();
+	
+	public static void main(String[] args) {
+	    listeGaz.add("hydrogène");
+	    listeGaz.add("hélium");
+	    listeGaz.add("CO2");
+	    listeGaz.add("Autre");
+	  }
+	
+	
+	@Override
+	public String receptionGaz(Date dateApparition, String localisation, int type, int importance) {
+		return localisation+" ("+dateApparition+") : "+type + " " +importance+"/3";
+	}
 
 }
